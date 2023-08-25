8934645226f2983ce1ac4ac38445c11e8b4b94ba
Randy Reddig <ydnar@shaderlab.com>
2023-08-25 12:57:36 -0700

    wit/internal/idarena: flesh out structs

--- wit/internal/idarena/wit.go
+++ wit/internal/idarena/wit.go
@@ -6,11 +6,24 @@ type WIT struct {
 	Packages   Arena[Package]
 }
 
+type Package struct {
+	Docs Docs `json:"docs"`
+}
+
 type World struct {
+	Docs Docs `json:"docs"`
 }
 
 type Interface struct {
+	Name string `json:"name"`
+	Docs Docs   `json:"docs"`
+}
+type Docs struct {
+	Contents *string `json:"contents"`
 }
 
-type Package struct {
+type Type struct {
+	Name string `json:"name"`
+	Docs Docs   `json:"docs"`
+	// TODO: Owner // typed ArenaID
 }
