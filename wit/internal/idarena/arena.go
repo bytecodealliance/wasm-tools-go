396c3699dfd95fd2fe1e8ba81ec30fc60fb1238f
Randy Reddig <ydnar@shaderlab.com>
2023-08-28 11:39:43 -0700

    wit/internal: experimenting

--- wit/internal/idarena/arena.go
+++ wit/internal/idarena/arena.go
@@ -5,6 +5,10 @@ type Arena[T any] struct {
 	Items   []T `json:"items"`
 }
 
+func (a Arena[T]) Item(i int) *T {
+	return &a.Items[i]
+}
+
 type ArenaID struct {
 	ArenaID int `json:"arena_id"`
 	Index   int `json:"idx"`
--- wit/internal/idarena/wit.go
+++ wit/internal/idarena/wit.go
@@ -1,6 +1,6 @@
 package idarena
 
-type WIT struct {
+type Resolve struct {
 	Worlds     Arena[World]     `json:"worlds"`
 	Interfaces Arena[Interface] `json:"interfaces"`
 	Packages   Arena[Package]   `json:"packages"`
@@ -14,17 +14,75 @@ type World struct {
 	Docs Docs `json:"docs"`
 }
 
-type Interface struct {
-	Name string `json:"name"`
-	Docs Docs   `json:"docs"`
+type WorldItem struct {
+	Interface *ArenaID  `json:"Interface,omitempty"`
+	Function  *Function `json:"Function,omitempty"`
+	Type      *ArenaID  `json:"Type,omitempty"`
 }
 
-type Type struct {
-	Name string `json:"name"`
-	Docs Docs   `json:"docs"`
-	// TODO: Owner // typed ArenaID
+type Interface struct {
+	Docs      Docs                `json:"docs"`
+	Name      *string             `json:"name"`
+	Types     map[string]ArenaID  `json:"types"`
+	Functions map[string]Function `json:"functions"`
+	Package   *ArenaID            `json:"package"`
+}
+
+/*
+pub enum Type {
+    Bool,
+    U8,
+    U16,
+    U32,
+    U64,
+    S8,
+    S16,
+    S32,
+    S64,
+    Float32,
+    Float64,
+    Char,
+    String,
+    Id(TypeId),
+}
+*/
+
+type Type interface {
+	isType()
+}
+
+type BoolType struct{}
+type U8Type struct{}
+
+type Function struct {
+	Docs    Docs
+	Name    string
+	Kind    FunctionKind
+	Params  []Param // Vec<(String, Type)>;
+	Results Results // enum
+}
+
+type FunctionKind struct {
+	Freestanding bool     `json:"freestanding,omitempty"`
+	Method       *ArenaID `json:"method,omitempty"`
+	Static       *ArenaID `json:"static,omitempty"`
+	Constructor  *ArenaID `json:"constructor,omitempty"`
 }
 
 type Docs struct {
 	Contents *string `json:"contents"`
+}
+
+type Param struct {
+	Name string
+	Type Type
+}
+
+type Results struct {
+	Named []Param
+	Anon  *ArenaID
+}
+
+func foo() {
+
 }
