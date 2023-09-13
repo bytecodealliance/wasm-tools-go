fbc428c53192f37fe223e3754312bf19d6b523f2
Randy Reddig <ydnar@shaderlab.com>
2023-09-12 17:25:26 -0700

    internal/codec: docs hygiene

--- internal/codec/constraints.go
+++ internal/codec/constraints.go
@@ -1,17 +1,21 @@
 package codec
 
+// Signed is the set of signed integer types supported by this package.
 type Signed interface {
 	int | int8 | int16 | int32 | int64
 }
 
+// Unsigned is the set of unsigned integer types supported by this package.
 type Unsigned interface {
 	uint | uint8 | uint16 | uint32 | uint64
 }
 
+// Integer is the set of integer types supported by this package.
 type Integer interface {
 	Signed | Unsigned
 }
 
+// Float is the set of integer types supported by this package.
 type Float interface {
 	float32 | float64
 }
