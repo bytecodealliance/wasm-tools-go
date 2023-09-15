11dedc9612059c770a296b22b6059415a3f5421d
Randy Reddig <ydnar@shaderlab.com>
2023-09-15 07:55:32 -0700

    wit: decode JSON string into package name
    TODO: add package name parsing for namespace:name@version

--- wit/codec.go
+++ wit/codec.go
@@ -190,6 +190,12 @@ func (c *packageCodec) DecodeField(dec codec.Decoder, name string) error {
 	return nil
 }
 
+// TODO: parse package name e.g. "wasi:clocks@1.0.2"
+func (c *PackageName) DecodeString(s string) error {
+	*c = PackageName(s)
+	return nil
+}
+
 // worldItemCodec translates typed WorldItem references into a WorldItem,
 // currently either an Interface or a TypeDef.
 type worldItemCodec struct {
