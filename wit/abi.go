b5534e38aa91449c677942dff7fc7c8eebf7b931
Randy Reddig <ydnar@shaderlab.com>
2023-10-02 17:55:45 -0600

    wit: add Despecializer interface and Despecialize function

--- wit/abi.go
+++ wit/abi.go
@@ -31,3 +31,27 @@ type _sized struct{}
 
 func (_sized) Size() uintptr  { panic("BUG: unimplemented") }
 func (_sized) Align() uintptr { panic("BUG: unimplemented") }
+
+// Despecializer is the interface implemented by any [TypeDefKind] that can
+// [despecialize] itself into another TypeDefKind. Examples include [Result],
+// which despecializes into a [Variant] with two cases, "ok" and "error".
+// See the [canonical ABI documentation] for more information.
+//
+// [despecialize]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#despecialization
+// [canonical ABI documentation]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#despecialization
+type Despecializer interface {
+	Despecialize() TypeDefKind
+}
+
+// Despecialize [despecializes] k if k implements [Despecializer].
+// Otherwise, it returns k unmodified.
+// See the [canonical ABI documentation] for more information.
+//
+// [despecializes]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#despecialization
+// [canonical ABI documentation]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#despecialization
+func Despecialize(k TypeDefKind) TypeDefKind {
+	if d, ok := k.(Despecializer); ok {
+		return d.Despecialize()
+	}
+	return k
+}
