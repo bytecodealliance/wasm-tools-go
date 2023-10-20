19a587df6c9714a55c2623832310465538c71675
Randy Reddig <randy.reddig@fastly.com>
2023-10-20 10:30:35 -0700

    abi: add test for alignment

--- abi/realloc_test.go
+++ abi/realloc_test.go
@@ -20,7 +20,8 @@ func TestRealloc(t *testing.T) {
 		{"nil", 0, 0, 1, 0, 0},
 		{"nil with align", 0, 0, 2, 0, 0},
 		{"align to 2", 1, 0, 2, 0, 2},
-		{"align to 8", 1, 0, 8, 0, 8},
+		{"align to 2", 1, 0, 2, 0, 2},
+		{"align to 4", 1, 0, 4, 0, 4},
 		{"align to 8", 3, 0, 8, 0, 8},
 		{"align to 8", 9, 0, 16, 0, 16},
 		{"alloc 100 bytes", 0, 0, 1, 100, sliceData(hundred)},
@@ -34,8 +35,11 @@ func TestRealloc(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := realloc(unsafePointer(tt.ptr), tt.size, tt.align, tt.newsize)
 			if (tt.want < threshold && uintptr(got) != tt.want) || (tt.want >= threshold && uintptr(got) < threshold) {
-				t.Errorf("Realloc(%d, %d, %d, %d): expected %d, got %d",
+				t.Errorf("realloc(%d, %d, %d, %d): expected %d, got %d",
 					tt.ptr, tt.size, tt.align, tt.newsize, tt.want, got)
+			}
+			if uintptr(got)%tt.align != 0 {
+				t.Errorf("expected ptr aligned to %d, got %d", tt.align, uintptr(got)&tt.align)
 			}
 			if uintptr(got) < threshold {
 				return // it didn’t allocate
