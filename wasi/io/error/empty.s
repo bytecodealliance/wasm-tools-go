82ce28fbf0e58cbce1b5724ec2d07d1e2e63da4d
Randy Reddig <randy.reddig@fastly.com>
2024-01-19 10:24:20 -0800

    wasi/io/error: fix empty.s

--- wasi/io/error/empty.s
+++ wasi/io/error/empty.s
@@ -1,2 +1,3 @@
-// This file exists to allow testing of this package without wasm.
+// This file exists for testing this package without WebAssembly,
+// allowing empty function bodies with a //go:wasmimport directive.
 // See https://pkg.go.dev/cmd/compile for more information.
