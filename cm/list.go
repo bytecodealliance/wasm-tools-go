9ed5680eb72af7bde08ad029057bea789f1e51ea
Randy Reddig <randy.reddig@fastly.com>
2024-01-22 11:17:57 -0800

    cm: NewList function to create a new List[T] from a pointer and len

--- cm/list.go
+++ cm/list.go
@@ -1,12 +1,22 @@
 package cm
 
-import "unsafe"
+import (
+	"unsafe"
+)
 
 // List represents a Component Model list<T>.
 // The binary representation of list<T> is similar to a Go slice minus the cap field.
 type List[T any] struct {
 	data *T
 	len  uint
+}
+
+// NewList returns a List[T] from data and len.
+func NewList[T any](data *T, len uint) List[T] {
+	return List[T]{
+		data: data,
+		len:  len,
+	}
 }
 
 // ToList returns a List[T] equivalent to the Go slice s.
