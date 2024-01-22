1c84738f62151053b9894deb0d418f5e933a6af1
Randy Reddig <randy.reddig@fastly.com>
2024-01-22 10:47:01 -0800

    wasi/cli/{stdin,stdout,stderr}: manually generated Go from WIT

---
+++ wasi/cli/stderr/stderr.wit.go
@@ -0,0 +1,13 @@
+package stdout
+
+import "github.com/ydnar/wasm-tools-go/wasi/io/streams"
+
+type OutputStream = streams.OutputStream
+
+// GetStderr represents the imported function "wasi:cli/stderr#get-stderr".
+func GetStderr() OutputStream {
+	return get_stderr()
+}
+
+//go:wasmimport wasi:cli/stderr@0.2.0-rc-2023-11-10 get-stderr
+func get_stderr() OutputStream
