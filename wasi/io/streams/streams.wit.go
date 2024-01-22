d52c0612d22f9967506228471fe5503be972bec5
Randy Reddig <randy.reddig@fastly.com>
2024-01-22 10:04:24 -0800

    wasi/io/streams: manually convert WIT to Go; WIP

--- wasi/io/streams/streams.wit.go
+++ wasi/io/streams/streams.wit.go
@@ -145,178 +145,236 @@ func (self InputStream) Subscribe() Pollable {
 //go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]input-stream.subscribe
 func (self InputStream) subscribe() Pollable
 
-/*
+// OutputStream represents the resource "wasi:io/streams.output-stream".
+//
+// An output bytestream.
+//
+// `output-stream`s are *non-blocking* to the extent practical on
+// underlying platforms. Except where specified otherwise, I/O operations also
+// always return promptly, after the number of bytes that can be written
+// promptly, which could even be zero. To wait for the stream to be ready to
+// accept data, the `subscribe` function to obtain a `pollable` which can be
+// polled for using `wasi:io/poll`.
+type OutputStream cm.Resource
 
-interface streams {
-    resource input-stream {
-        // Create a `pollable` which will resolve once either the specified stream
-        // has bytes available to read or the other end of the stream has been
-        // closed.
-        // The created `pollable` is a child resource of the `input-stream`.
-        // Implementations may trap if the `input-stream` is dropped before
-        // all derived `pollable`s created with this function are dropped.
-        subscribe: func() -> pollable;
-    }
-
-
-    // An output bytestream.
-    //
-    // `output-stream`s are *non-blocking* to the extent practical on
-    // underlying platforms. Except where specified otherwise, I/O operations also
-    // always return promptly, after the number of bytes that can be written
-    // promptly, which could even be zero. To wait for the stream to be ready to
-    // accept data, the `subscribe` function to obtain a `pollable` which can be
-    // polled for using `wasi:io/poll`.
-    resource output-stream {
-        // Check readiness for writing. This function never blocks.
-        //
-        // Returns the number of bytes permitted for the next call to `write`,
-        // or an error. Calling `write` with more bytes than this function has
-        // permitted will trap.
-        //
-        // When this function returns 0 bytes, the `subscribe` pollable will
-        // become ready when this function will report at least 1 byte, or an
-        // error.
-        check-write: func() -> result<u64, stream-error>;
-
-        // Perform a write. This function never blocks.
-        //
-        // Precondition: check-write gave permit of Ok(n) and contents has a
-        // length of less than or equal to n. Otherwise, this function will trap.
-        //
-        // returns Err(closed) without writing if the stream has closed since
-        // the last call to check-write provided a permit.
-        write: func(
-            contents: list<u8>
-        ) -> result<_, stream-error>;
-
-        // Perform a write of up to 4096 bytes, and then flush the stream. Block
-        // until all of these operations are complete, or an error occurs.
-        //
-        // This is a convenience wrapper around the use of `check-write`,
-        // `subscribe`, `write`, and `flush`, and is implemented with the
-        // following pseudo-code:
-        //
-        // ```text
-        // let pollable = this.subscribe();
-        // while !contents.is_empty() {
-        //     // Wait for the stream to become writable
-        //     pollable.block();
-        //     let Ok(n) = this.check-write(); // eliding error handling
-        //     let len = min(n, contents.len());
-        //     let (chunk, rest) = contents.split_at(len);
-        //     this.write(chunk  );            // eliding error handling
-        //     contents = rest;
-        // }
-        // this.flush();
-        // // Wait for completion of `flush`
-        // pollable.block();
-        // // Check for any errors that arose during `flush`
-        // let _ = this.check-write();         // eliding error handling
-        // ```
-        blocking-write-and-flush: func(
-            contents: list<u8>
-        ) -> result<_, stream-error>;
-
-        // Request to flush buffered output. This function never blocks.
-        //
-        // This tells the output-stream that the caller intends any buffered
-        // output to be flushed. the output which is expected to be flushed
-        // is all that has been passed to `write` prior to this call.
-        //
-        // Upon calling this function, the `output-stream` will not accept any
-        // writes (`check-write` will return `ok(0)`) until the flush has
-        // completed. The `subscribe` pollable will become ready when the
-        // flush has completed and the stream can accept more writes.
-        flush: func() -> result<_, stream-error>;
-
-        // Request to flush buffered output, and block until flush completes
-        // and stream is ready for writing again.
-        blocking-flush: func() -> result<_, stream-error>;
-
-        // Create a `pollable` which will resolve once the output-stream
-        // is ready for more writing, or an error has occured. When this
-        // pollable is ready, `check-write` will return `ok(n)` with n>0, or an
-        // error.
-        //
-        // If the stream is closed, this pollable is always ready immediately.
-        //
-        // The created `pollable` is a child resource of the `output-stream`.
-        // Implementations may trap if the `output-stream` is dropped before
-        // all derived `pollable`s created with this function are dropped.
-        subscribe: func() -> pollable;
-
-        // Write zeroes to a stream.
-        //
-        // This should be used precisely like `write` with the exact same
-        // preconditions (must use check-write first), but instead of
-        // passing a list of bytes, you simply pass the number of zero-bytes
-        // that should be written.
-        write-zeroes: func(
-            // The number of zero-bytes to write
-            len: u64
-        ) -> result<_, stream-error>;
-
-        // Perform a write of up to 4096 zeroes, and then flush the stream.
-        // Block until all of these operations are complete, or an error
-        // occurs.
-        //
-        // This is a convenience wrapper around the use of `check-write`,
-        // `subscribe`, `write-zeroes`, and `flush`, and is implemented with
-        // the following pseudo-code:
-        //
-        // ```text
-        // let pollable = this.subscribe();
-        // while num_zeroes != 0 {
-        //     // Wait for the stream to become writable
-        //     pollable.block();
-        //     let Ok(n) = this.check-write(); // eliding error handling
-        //     let len = min(n, num_zeroes);
-        //     this.write-zeroes(len);         // eliding error handling
-        //     num_zeroes -= len;
-        // }
-        // this.flush();
-        // // Wait for completion of `flush`
-        // pollable.block();
-        // // Check for any errors that arose during `flush`
-        // let _ = this.check-write();         // eliding error handling
-        // ```
-        blocking-write-zeroes-and-flush: func(
-            // The number of zero-bytes to write
-            len: u64
-        ) -> result<_, stream-error>;
-
-        // Read from one stream and write to another.
-        //
-        // The behavior of splice is equivelant to:
-        // 1. calling `check-write` on the `output-stream`
-        // 2. calling `read` on the `input-stream` with the smaller of the
-        // `check-write` permitted length and the `len` provided to `splice`
-        // 3. calling `write` on the `output-stream` with that read data.
-        //
-        // Any error reported by the call to `check-write`, `read`, or
-        // `write` ends the splice and reports that error.
-        //
-        // This function returns the number of bytes transferred; it may be less
-        // than `len`.
-        splice: func(
-            // The stream to read from
-            src: borrow<input-stream>,
-            // The number of bytes to splice
-            len: u64,
-        ) -> result<u64, stream-error>;
-
-        // Read from one stream and write to another, with blocking.
-        //
-        // This is similar to `splice`, except that it blocks until the
-        // `output-stream` is ready for writing, and the `input-stream`
-        // is ready for reading, before performing the `splice`.
-        blocking-splice: func(
-            // The stream to read from
-            src: borrow<input-stream>,
-            // The number of bytes to splice
-            len: u64,
-        ) -> result<u64, stream-error>;
-    }
+// CheckWrite represents the method "wasi:io/streams.output-stream.check-write".
+//
+// Check readiness for writing. This function never blocks.
+//
+// Returns the number of bytes permitted for the next call to `write`,
+// or an error. Calling `write` with more bytes than this function has
+// permitted will trap.
+//
+// When this function returns 0 bytes, the `subscribe` pollable will
+// become ready when this function will report at least 1 byte, or an
+// error.
+func (self OutputStream) CheckWrite() cm.OKSizedResult[uint64, StreamError] {
+	var ret cm.OKSizedResult[uint64, StreamError]
+	self.check_write(&ret)
+	return ret
 }
-*/
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.check-write
+func (self OutputStream) check_write(ret *cm.OKSizedResult[uint64, StreamError])
+
+// Write represents the method "wasi:io/streams.output-stream.write".
+//
+// Perform a write. This function never blocks.
+//
+// Precondition: check-write gave permit of Ok(n) and contents has a
+// length of less than or equal to n. Otherwise, this function will trap.
+//
+// returns Err(closed) without writing if the stream has closed since
+// the last call to check-write provided a permit.
+func (self OutputStream) Write(contents cm.List[uint8]) cm.ErrSizedResult[struct{}, StreamError] {
+	var ret cm.ErrSizedResult[struct{}, StreamError]
+	self.write(contents, &ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.write
+func (self OutputStream) write(contents cm.List[uint8], ret *cm.ErrSizedResult[struct{}, StreamError])
+
+// BlockingWriteAndFlush represents the method "wasi:io/streams.output-stream.blocking-write-and-flush".
+//
+// Perform a write of up to 4096 bytes, and then flush the stream. Block
+// until all of these operations are complete, or an error occurs.
+//
+// This is a convenience wrapper around the use of `check-write`,
+// `subscribe`, `write`, and `flush`, and is implemented with the
+// following pseudo-code:
+//
+// ```text
+// let pollable = this.subscribe();
+//
+//	while !contents.is_empty() {
+//	    // Wait for the stream to become writable
+//	    pollable.block();
+//	    let Ok(n) = this.check-write(); // eliding error handling
+//	    let len = min(n, contents.len());
+//	    let (chunk, rest) = contents.split_at(len);
+//	    this.write(chunk  );            // eliding error handling
+//	    contents = rest;
+//	}
+//
+// this.flush();
+// // Wait for completion of `flush`
+// pollable.block();
+// // Check for any errors that arose during `flush`
+// let _ = this.check-write();         // eliding error handling
+// ```
+func (self OutputStream) BlockingWriteAndFlush(contents cm.List[uint8]) cm.ErrSizedResult[struct{}, StreamError] {
+	var ret cm.ErrSizedResult[struct{}, StreamError]
+	self.blocking_write_and_flush(contents, &ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.blocking-write-and-flush
+func (self OutputStream) blocking_write_and_flush(contents cm.List[uint8], ret *cm.ErrSizedResult[struct{}, StreamError])
+
+// Flush represents the method "wasi:io/streams.output-stream.flush".
+//
+// Request to flush buffered output. This function never blocks.
+//
+// This tells the output-stream that the caller intends any buffered
+// output to be flushed. the output which is expected to be flushed
+// is all that has been passed to `write` prior to this call.
+//
+// Upon calling this function, the `output-stream` will not accept any
+// writes (`check-write` will return `ok(0)`) until the flush has
+// completed. The `subscribe` pollable will become ready when the
+// flush has completed and the stream can accept more writes.
+func (self OutputStream) Flush() cm.ErrSizedResult[struct{}, StreamError] {
+	var ret cm.ErrSizedResult[struct{}, StreamError]
+	self.flush(&ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.flush
+func (self OutputStream) flush(ret *cm.ErrSizedResult[struct{}, StreamError])
+
+// BlockingFlush represents the method "wasi:io/streams.output-stream.blocking-flush".
+//
+// Request to flush buffered output, and block until flush completes
+// and stream is ready for writing again.
+func (self OutputStream) BlockingFlush() cm.ErrSizedResult[struct{}, StreamError] {
+	var ret cm.ErrSizedResult[struct{}, StreamError]
+	self.blocking_flush(&ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.blocking-flush
+func (self OutputStream) blocking_flush(ret *cm.ErrSizedResult[struct{}, StreamError])
+
+// Subscribe represents the method "wasi:io/streams.output-stream.subscribe".
+//
+// Create a `pollable` which will resolve once the output-stream
+// is ready for more writing, or an error has occured. When this
+// pollable is ready, `check-write` will return `ok(n)` with n>0, or an
+// error.
+//
+// If the stream is closed, this pollable is always ready immediately.
+//
+// The created `pollable` is a child resource of the `output-stream`.
+// Implementations may trap if the `output-stream` is dropped before
+// all derived `pollable`s created with this function are dropped.
+func (self OutputStream) Subscribe() Pollable {
+	return self.subscribe()
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.subscribe
+func (self OutputStream) subscribe() Pollable
+
+// WriteZeroes represents the method "wasi:io/streams.output-stream.write-zeroes".
+//
+// Write zeroes to a stream.
+//
+// This should be used precisely like `write` with the exact same
+// preconditions (must use check-write first), but instead of
+// passing a list of bytes, you simply pass the number of zero-bytes
+// that should be written.
+func (self OutputStream) WriteZeroes(len uint64) cm.ErrSizedResult[struct{}, StreamError] {
+	var ret cm.ErrSizedResult[struct{}, StreamError]
+	self.write_zeroes(len, &ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.write-zeroes
+func (self OutputStream) write_zeroes(len uint64, ret *cm.ErrSizedResult[struct{}, StreamError])
+
+// BlockingWriteZeroesAndFlush represents the method "wasi:io/streams.output-stream.blocking-write-zeroes-and-flush".
+//
+// Perform a write of up to 4096 zeroes, and then flush the stream.
+// Block until all of these operations are complete, or an error
+// occurs.
+//
+// This is a convenience wrapper around the use of `check-write`,
+// `subscribe`, `write-zeroes`, and `flush`, and is implemented with
+// the following pseudo-code:
+//
+// ```text
+// let pollable = this.subscribe();
+//
+//	while num_zeroes != 0 {
+//	    // Wait for the stream to become writable
+//	    pollable.block();
+//	    let Ok(n) = this.check-write(); // eliding error handling
+//	    let len = min(n, num_zeroes);
+//	    this.write-zeroes(len);         // eliding error handling
+//	    num_zeroes -= len;
+//	}
+//
+// this.flush();
+// // Wait for completion of `flush`
+// pollable.block();
+// // Check for any errors that arose during `flush`
+// let _ = this.check-write();         // eliding error handling
+// ```
+func (self OutputStream) BlockingWriteZeroesAndFlush(len uint64) cm.ErrSizedResult[struct{}, StreamError] {
+	var ret cm.ErrSizedResult[struct{}, StreamError]
+	self.blocking_write_zeroes_and_flush(len, &ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.blocking-write-zeroes-and-flush
+func (self OutputStream) blocking_write_zeroes_and_flush(len uint64, ret *cm.ErrSizedResult[struct{}, StreamError])
+
+// Splice represents the method "wasi:io/streams.output-stream.splice".
+//
+// Read from one stream and write to another.
+//
+// The behavior of splice is equivelant to:
+// 1. calling `check-write` on the `output-stream`
+// 2. calling `read` on the `input-stream` with the smaller of the
+// `check-write` permitted length and the `len` provided to `splice`
+// 3. calling `write` on the `output-stream` with that read data.
+//
+// Any error reported by the call to `check-write`, `read`, or
+// `write` ends the splice and reports that error.
+//
+// This function returns the number of bytes transferred; it may be less
+// than `len`.
+func (self OutputStream) Splice(src InputStream, len uint64) cm.OKSizedResult[uint64, StreamError] {
+	var ret cm.OKSizedResult[uint64, StreamError]
+	self.splice(src, len, &ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.splice
+func (self OutputStream) splice(src InputStream, len uint64, ret *cm.OKSizedResult[uint64, StreamError])
+
+// BlockingSplice represents the method "wasi:io/streams.output-stream.blocking-splice".
+//
+// Read from one stream and write to another, with blocking.
+//
+// This is similar to `splice`, except that it blocks until the
+// `output-stream` is ready for writing, and the `input-stream`
+// is ready for reading, before performing the `splice`.
+func (self OutputStream) BlockingSplice(src InputStream, len uint64) cm.OKSizedResult[uint64, StreamError] {
+	var ret cm.OKSizedResult[uint64, StreamError]
+	self.blocking_splice(src, len, &ret)
+	return ret
+}
+
+//go:wasmimport wasi:io/streams@0.2.0-rc-2023-11-10 [method]output-stream.blocking-splice
+func (self OutputStream) blocking_splice(src InputStream, len uint64, ret *cm.OKSizedResult[uint64, StreamError])
