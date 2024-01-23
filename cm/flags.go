a194aea089aa3352a96e23ddf0a4c20f335cfea1
Randy Reddig <randy.reddig@fastly.com>
2024-01-22 17:55:19 -0800

    cm: Flag and Flags types; WIP

--- cm/flags.go
+++ cm/flags.go
@@ -2,34 +2,46 @@ package cm
 
 import "unsafe"
 
-type (
-	Flags8  uint8
-	Flags16 uint16
-	Flags32 uint32
-	Flags64 uint64
-	Flags96 [3]uint32
-)
+// Flag represents an individual flag. The value of a Flag
+// is index of the bit into a Flags value.
+//
+// The intended use is as a separate named type:
+//
+//	type MyFlag cm.Flag
+//	type MyFlags struct { cm.Flags[uint8, MyFlag] }
+type Flag uint
 
-// TODO: necessary?
-func (f Flags8) Is(flag Flags8) bool {
-	return f&flag != 0
+// FIXME(ydnar): this is silly
+type flagsShape interface {
+	uint8 | uint16 | uint32 |
+		[2]uint32 | [3]uint32 | [4]uint32 | [5]uint32 | [6]uint32 | [7]uint32 |
+		[8]uint32 | [9]uint32 | [10]uint32 | [11]uint32 | [12]uint32 | [13]uint32 | [14]uint32 | [15]uint32 |
+		[16]uint32 | [17]uint32 | [18]uint32 | [19]uint32 | [20]uint32 | [21]uint32 | [22]uint32 | [23]uint32 |
+		[24]uint32 | [25]uint32 | [26]uint32 | [27]uint32 | [28]uint32 | [29]uint32 | [30]uint32 | [31]uint32
 }
 
-type Flags[Shape any] struct {
+// Flags represents a bitfield of multiple flags.
+// Shape must be either uint8, uint16, uint32, or an
+// array of uint32 large enough to contain the max
+// value of the associated Flag type.
+type Flags[Shape flagsShape, Flag ~uint] struct {
 	data Shape
 }
 
-func (f *Flags[Shape]) IsSet(bit uint) bool {
-	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), bit>>3))
-	return *ptr&(1<<(bit&0b111)) != 0
+// IsSet returns true if flag is set.
+func (f *Flags[Shape, Flag]) IsSet(flag Flag) bool {
+	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), flag>>3))
+	return *ptr&(1<<(flag&0b111)) != 0
 }
 
-func (f *Flags[Shape]) Set(bit uint) {
-	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), bit>>3))
-	*ptr |= 1 << (bit & 0b111)
+// Set sets the bit indexed by flag to 1.
+func (f *Flags[Shape, Flag]) Set(flag Flag) {
+	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), flag>>3))
+	*ptr |= 1 << (flag & 0b111)
 }
 
-func (f *Flags[Shape]) Clear(bit uint) {
-	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), bit>>3))
-	*ptr &^= 1 << (bit & 0b111)
+// Clear clears the bit indexed by flag.
+func (f *Flags[Shape, Flag]) Clear(flag Flag) {
+	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), flag>>3))
+	*ptr &^= 1 << (flag & 0b111)
 }
--- cm/flags_test.go
+++ cm/flags_test.go
@@ -6,31 +6,45 @@ import (
 )
 
 func TestFlags(t *testing.T) {
-	var flags1 Flags[[2]uint32]
-	flags1.Set(0)
+	type MyFlag Flag
+	const (
+		F0 MyFlag = iota
+		F1
+
+		F32 MyFlag = 32
+		F63 MyFlag = 63
+		F95 MyFlag = 95
+	)
+
+	var flags1 struct {
+		Flags[[2]uint32, MyFlag]
+	}
+	flags1.Set(F0)
 	flags1.Set(63)
-	if !flags1.IsSet(0) {
+	if !flags1.IsSet(F0) {
 		t.Errorf("expected bit 0 to be set")
 	}
-	if !flags1.IsSet(63) {
-		t.Errorf("expected bit 63 to be set")
+	if !flags1.IsSet(F63) {
+		t.Errorf("expected bit %d to be set", F63)
 	}
-	if flags1.IsSet(32) {
-		t.Errorf("expected bit 32 to not be set")
+	if flags1.IsSet(F32) {
+		t.Errorf("expected bit %d to not be set", F32)
 	}
 	fmt.Printf("flags1: %b\n", flags1.data)
 
-	var flags2 Flags[[3]uint32]
-	flags2.Set(1)
-	flags2.Set(95)
-	if !flags2.IsSet(1) {
-		t.Errorf("expected bit 0 to be set")
+	var flags2 struct {
+		Flags[[3]uint32, MyFlag]
 	}
-	if !flags2.IsSet(95) {
-		t.Errorf("expected bit 95 to be set")
+	flags2.Set(F1)
+	flags2.Set(F95)
+	if !flags2.IsSet(F1) {
+		t.Errorf("expected bit %d to be set", F1)
+	}
+	if !flags2.IsSet(F95) {
+		t.Errorf("expected bit %d to be set", F95)
 	}
 	if flags2.IsSet(0) {
-		t.Errorf("expected bit 0 to not be set")
+		t.Errorf("expected bit %d to not be set", F0)
 	}
 	fmt.Printf("flags2: %b\n", flags2.data)
 }
