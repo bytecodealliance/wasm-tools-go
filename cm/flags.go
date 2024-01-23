8d0739cbb9b60dcc71bc6f27add98f59b6399184
Randy Reddig <randy.reddig@fastly.com>
2024-01-23 11:58:29 -0800

    cm/flags: add Flags8, Flags16, Flags32, Flags64 types as simpler const-able types

--- cm/flags.go
+++ cm/flags.go
@@ -1,6 +1,8 @@
 package cm
 
-import "unsafe"
+import (
+	"unsafe"
+)
 
 // Flag represents an individual flag. The value of a Flag
 // is index of the bit into a Flags value.
@@ -29,13 +31,13 @@ type Flags[Shape flagsShape, Flag ~uint] struct {
 	data Shape
 }
 
-// IsSet returns true if flag is set.
-func (f *Flags[Shape, Flag]) IsSet(flag Flag) bool {
+// Is returns true if flag is set.
+func (f *Flags[Shape, Flag]) Is(flag Flag) bool {
 	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), flag>>3))
 	return *ptr&(1<<(flag&0b111)) != 0
 }
 
-// Set sets the bit indexed by flag to 1.
+// Set sets or clears the bit indexed by flag.
 func (f *Flags[Shape, Flag]) Set(flag Flag) {
 	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), flag>>3))
 	*ptr |= 1 << (flag & 0b111)
@@ -45,4 +47,76 @@ func (f *Flags[Shape, Flag]) Set(flag Flag) {
 func (f *Flags[Shape, Flag]) Clear(flag Flag) {
 	ptr := (*byte)(unsafe.Add(unsafe.Pointer(f), flag>>3))
 	*ptr &^= 1 << (flag & 0b111)
+}
+
+// Flags8 represents a flags value with 1-8 unique flags.
+type Flags8[Flag ~uint] uint8
+
+// Is returns true if flag is set.
+func (f *Flags8[Flag]) Is(flag Flag) bool {
+	return *f&(1<<flag) != 0
+}
+
+// Set sets or clears the bit indexed by flag.
+func (f *Flags8[Flag]) Set(flag Flag) {
+	*f |= 1 << flag
+}
+
+// Clear clears the bit indexed by flag.
+func (f *Flags8[Flag]) Clear(flag Flag) {
+	*f &^= 1 << flag
+}
+
+// Flags16 represents a flags value with 9-16 unique flags.
+type Flags16[Flag ~uint] uint16
+
+// Is returns true if flag is set.
+func (f *Flags16[Flag]) Is(flag Flag) bool {
+	return *f&(1<<flag) != 0
+}
+
+// Set sets or clears the bit indexed by flag.
+func (f *Flags16[Flag]) Set(flag Flag) {
+	*f |= 1 << flag
+}
+
+// Clear clears the bit indexed by flag.
+func (f *Flags16[Flag]) Clear(flag Flag) {
+	*f &^= 1 << flag
+}
+
+// Flags32 represents a flags value with 17-32 unique flags.
+type Flags32[Flag ~uint] uint32
+
+// Is returns true if flag is set.
+func (f *Flags32[Flag]) Is(flag Flag) bool {
+	return *f&(1<<flag) != 0
+}
+
+// Set sets or clears the bit indexed by flag.
+func (f *Flags32[Flag]) Set(flag Flag) {
+	*f |= 1 << flag
+}
+
+// Clear clears the bit indexed by flag.
+func (f *Flags32[Flag]) Clear(flag Flag) {
+	*f &^= 1 << flag
+}
+
+// Flags64 represents a flags value with 33-64 unique flags.
+type Flags64[Flag ~uint] uint64
+
+// Is returns true if flag is set.
+func (f *Flags64[Flag]) Is(flag Flag) bool {
+	return *f&(1<<flag) != 0
+}
+
+// Set sets or clears the bit indexed by flag.
+func (f *Flags64[Flag]) Set(flag Flag) {
+	*f |= 1 << flag
+}
+
+// Clear clears the bit indexed by flag.
+func (f *Flags64[Flag]) Clear(flag Flag) {
+	*f &^= 1 << flag
 }
--- cm/flags_test.go
+++ cm/flags_test.go
@@ -21,13 +21,13 @@ func TestFlags(t *testing.T) {
 	}
 	flags1.Set(F0)
 	flags1.Set(63)
-	if !flags1.IsSet(F0) {
+	if !flags1.Is(F0) {
 		t.Errorf("expected bit 0 to be set")
 	}
-	if !flags1.IsSet(F63) {
+	if !flags1.Is(F63) {
 		t.Errorf("expected bit %d to be set", F63)
 	}
-	if flags1.IsSet(F32) {
+	if flags1.Is(F32) {
 		t.Errorf("expected bit %d to not be set", F32)
 	}
 	fmt.Printf("flags1: %b\n", flags1.data)
@@ -37,13 +37,13 @@ func TestFlags(t *testing.T) {
 	}
 	flags2.Set(F1)
 	flags2.Set(F95)
-	if !flags2.IsSet(F1) {
+	if !flags2.Is(F1) {
 		t.Errorf("expected bit %d to be set", F1)
 	}
-	if !flags2.IsSet(F95) {
+	if !flags2.Is(F95) {
 		t.Errorf("expected bit %d to be set", F95)
 	}
-	if flags2.IsSet(0) {
+	if flags2.Is(0) {
 		t.Errorf("expected bit %d to not be set", F0)
 	}
 	fmt.Printf("flags2: %b\n", flags2.data)
