6a59d216a97d887c460864170f4c23453fe4cca0
Randy Reddig <ydnar@shaderlab.com>
2024-02-11 21:17:49 -0800

    cm: streamline Variant and Result types
    - Replaced tuple-return OK and Err methods with pointers that can be nil
    - Cut down Variant types, added new top-level Case function for typed Variant

--- cm/result.go
+++ cm/result.go
@@ -1,5 +1,7 @@
 package cm
 
+import "unsafe"
+
 const (
 	// ResultOK represents the OK case of a result.
 	ResultOK = false
@@ -8,76 +10,57 @@ const (
 	ResultErr = true
 )
 
-// OKResult represents a result with only an OK type.
-type OKResult[OK any] struct {
-	Result[OK, OK, struct{}]
-}
-
-// ErrResult represents a result with only an error type.
-type ErrResult[Err any] struct {
-	Result[Err, struct{}, Err]
-}
-
-// OKSizedResult represents a result sized to hold the OK type.
-// The size of the OK type must be greater than or equal to the size of the Err type.
-// For results with two zero-length types, use UnsizedResult.
-type OKSizedResult[OK any, Err any] struct {
-	Result[OK, OK, Err]
-}
-
-// ErrSizedResult represents a result sized to hold the Err type.
-// The size of the Err type must be greater than or equal to the size of the OK type.
-// For results with two zero-length types, use UnsizedResult.
-type ErrSizedResult[OK any, Err any] struct {
-	Result[Err, OK, Err]
-}
-
 // Result is a tagged union representing either the OK type or the Err type.
 // Either OK or Err must have non-zero size, e.g. both cannot be struct{} or a zero-length array.
-// For results with two zero-length types, use UnsizedResult.
-// For results with no associated types, use UntypedResult.
-type Result[Shape any, OK any, Err any] struct {
-	v Variant2[Shape, OK, Err]
+// For results with zero-sized or no associated types, use UntypedResult.
+type Result[Shape, OK, Err any] struct {
+	isErr bool
+	_     [0]OK
+	_     [0]Err
+	data  Shape
+}
+
+// OKResult returns an OK [Result] with GC shape Shape and type OK and Err.
+func OKResult[Shape, OK, Err any](ok OK) Result[Shape, OK, Err] {
+	var r Result[Shape, OK, Err]
+	if BoundsCheck && unsafe.Sizeof(ok) > unsafe.Sizeof(r.data) {
+		panic("OKResult: size of requested type greater than size of data type")
+	}
+	r.isErr = ResultOK
+	*((*OK)(unsafe.Pointer(&r.data))) = ok
+	return r
+}
+
+// ErrResult returns an error [Error] with GC shape Shape and type OK and Err.
+func ErrResult[Shape, OK, Err any](err Err) Result[Shape, OK, Err] {
+	var r Result[Shape, OK, Err]
+	if BoundsCheck && unsafe.Sizeof(err) > unsafe.Sizeof(r.data) {
+		panic("ErrResult: size of requested type greater than size of data type")
+	}
+	r.isErr = ResultErr
+	*((*Err)(unsafe.Pointer(&r.data))) = err
+	return r
 }
 
 // IsErr returns true if r represents the error case.
-func (r *Result[S, OK, Err]) IsErr() bool {
-	return r.v.tag
+func (r *Result[Shape, OK, Err]) IsErr() bool {
+	return r.isErr
 }
 
-// SetOK stores the OK value in r.
-func (r *Result[S, OK, Err]) SetOK(ok OK) {
-	r.v.Set0(ok)
+// OK returns a non-nil *OK pointer if r represents the OK case.
+// If r represents an error, then it returns nil.
+func (r *Result[Shape, OK, Err]) OK() *OK {
+	if r.isErr {
+		return nil
+	}
+	return (*OK)(unsafe.Pointer(&r.data))
 }
 
-// SetErr stores the error value in r.
-func (r *Result[S, OK, Err]) SetErr(err Err) {
-	r.v.Set1(err)
+// Err returns a non-nil *Err pointer if r represents the error case.
+// If r represents the OK case, then it returns nil.
+func (r *Result[Shape, OK, Err]) Err() *Err {
+	if !r.isErr {
+		return nil
+	}
+	return (*Err)(unsafe.Pointer(&r.data))
 }
-
-// OK returns the OK value for r and true if r represents the OK case.
-// If r represents an error, then the zero value of OK is returned.
-func (r *Result[S, OK, Err]) OK() (ok OK, isOK bool) {
-	return r.v.Case0()
-}
-
-// Err returns the error value for r and true if r represents the error case.
-// If r represents the OK case, then the zero value of Err is returned.
-func (r *Result[S, OK, Err]) Err() (err Err, isErr bool) {
-	return r.v.Case1()
-}
-
-// UnsizedResult is a tagged union representing either the OK type or the Err type.
-// Both OK or Err must have zero size, e.g. both must be struct{} or a zero-length array.
-// For results with one or more non-zero length types, use Result.
-// For results with no associated types, use UntypedResult.
-type UnsizedResult[OK any, Err any] UnsizedVariant2[OK, Err]
-
-// IsErr returns true if r represents the error case.
-func (r *UnsizedResult[OK, Err]) IsErr() bool {
-	return bool(*r)
-}
-
-// UntypedResult represents an untyped Component Model result, e.g.
-// result or result<_, _>. The OK and Err types are both struct{}.
-type UntypedResult = UnsizedResult[struct{}, struct{}]
--- cm/result_test.go
+++ cm/result_test.go
@@ -7,16 +7,14 @@ import (
 
 var (
 	_ result[string, bool] = &Result[string, string, bool]{}
-	_ result[string, bool] = &OKSizedResult[string, bool]{}
-	_ result[bool, string] = &ErrSizedResult[bool, string]{}
+	_ result[string, bool] = &Result[string, string, bool]{}
+	_ result[bool, string] = &Result[string, bool, string]{}
 )
 
 type result[OK, Err any] interface {
 	IsErr() bool
-	SetOK(OK)
-	SetErr(Err)
-	OK() (ok OK, isOK bool)
-	Err() (err Err, isErr bool)
+	OK() *OK
+	Err() *Err
 }
 
 func TestResultLayout(t *testing.T) {
@@ -29,13 +27,6 @@ func TestResultLayout(t *testing.T) {
 		size   uintptr
 		offset uintptr
 	}{
-		{"result", UntypedResult(false), 1, 0},
-		{"ok", UntypedResult(ResultOK), 1, 0},
-		{"err", UntypedResult(ResultErr), 1, 0},
-
-		{"result<_, _>", UnsizedResult[struct{}, struct{}](false), 1, 0},
-		{"result<[0]u8, _>", UnsizedResult[[0]byte, struct{}](false), 1, 0},
-
 		{"result<string, string>", Result[string, string, string]{}, sizePlusAlignOf[string](), ptrSize},
 		{"result<bool, string>", Result[string, bool, string]{}, sizePlusAlignOf[string](), ptrSize},
 		{"result<string, _>", Result[string, string, struct{}]{}, sizePlusAlignOf[string](), ptrSize},
@@ -49,15 +40,15 @@ func TestResultLayout(t *testing.T) {
 		{"result<u32, u8>", Result[uint32, uint32, uint8]{}, 8, alignOf[uint32]()},
 		{"result<[9]u8, u64>", Result[[9]byte, [9]byte, uint64]{}, 24, alignOf[uint64]()},
 
-		{"result<string, _>", OKResult[string]{}, sizePlusAlignOf[string](), ptrSize},
-		{"result<string, _>", OKSizedResult[string, struct{}]{}, sizePlusAlignOf[string](), ptrSize},
-		{"result<string, bool>", OKSizedResult[string, bool]{}, sizePlusAlignOf[string](), ptrSize},
-		{"result<[9]u8, u64>", OKSizedResult[[9]byte, uint64]{}, 24, alignOf[uint64]()},
+		{"result<string, _>", Result[string, string, struct{}]{}, sizePlusAlignOf[string](), ptrSize},
+		{"result<string, _>", Result[string, string, struct{}]{}, sizePlusAlignOf[string](), ptrSize},
+		{"result<string, bool>", Result[string, string, bool]{}, sizePlusAlignOf[string](), ptrSize},
+		{"result<[9]u8, u64>", Result[[9]byte, [9]byte, uint64]{}, 24, alignOf[uint64]()},
 
-		{"result<_, string>", ErrResult[string]{}, sizePlusAlignOf[string](), ptrSize},
-		{"result<_, string>", ErrSizedResult[struct{}, string]{}, sizePlusAlignOf[string](), ptrSize},
-		{"result<bool, string>", ErrSizedResult[bool, string]{}, sizePlusAlignOf[string](), ptrSize},
-		{"result<u64, [9]u8>", ErrSizedResult[uint64, [9]byte]{}, 24, alignOf[uint64]()},
+		{"result<_, string>", Result[string, struct{}, string]{}, sizePlusAlignOf[string](), ptrSize},
+		{"result<_, string>", Result[string, struct{}, string]{}, sizePlusAlignOf[string](), ptrSize},
+		{"result<bool, string>", Result[string, bool, string]{}, sizePlusAlignOf[string](), ptrSize},
+		{"result<u64, [9]u8>", Result[[9]byte, uint64, [9]byte]{}, 24, alignOf[uint64]()},
 	}
 
 	for _, tt := range tests {
@@ -66,8 +57,8 @@ func TestResultLayout(t *testing.T) {
 			if got, want := tt.r.Size(), tt.size; got != want {
 				t.Errorf("(%s).Size() == %v, expected %v", typ, got, want)
 			}
-			if got, want := tt.r.ValOffset(), tt.offset; got != want {
-				t.Errorf("(%s).ValOffset() == %v, expected %v", typ, got, want)
+			if got, want := tt.r.DataOffset(), tt.offset; got != want {
+				t.Errorf("(%s).DataOffset() == %v, expected %v", typ, got, want)
 			}
 		})
 	}
