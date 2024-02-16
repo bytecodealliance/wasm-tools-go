ca13fcb2ef810c7bded450636d28f5f77069f490
Randy Reddig <randy.reddig@fastly.com>
2024-02-16 14:47:05 -0800

    wit: add Pointer type

--- wit/resolve.go
+++ wit/resolve.go
@@ -305,6 +305,34 @@ type _typeDefKind struct{}
 
 func (_typeDefKind) isTypeDefKind() {}
 
+// Pointer represents a pointer to a WIT type.
+// It is only used for ABI representation, e.g. pointers to function parameters or return values.
+type Pointer struct {
+	_typeDefKind
+	Type Type
+}
+
+// Size returns the [ABI byte size] for [Pointer].
+//
+// [ABI byte size]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#size
+func (*Pointer) Size() uintptr { return 4 }
+
+// Align returns the [ABI byte alignment] for [Pointer].
+//
+// [ABI byte alignment]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#alignment
+func (*Pointer) Align() uintptr { return 4 }
+
+// HasPointer returns whether the [ABI] representation of [Pointer] contains a pointer.
+// This always returns true.
+//
+// [ABI]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md
+func (*Pointer) HasPointer() bool { return true }
+
+// Flat returns the [flattened] ABI representation of [Pointer].
+//
+// [flattened]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#flattening
+func (*Pointer) Flat() []Type { return []Type{U32{}} }
+
 // Record represents a WIT [record type], akin to a struct.
 // It implements the [Node], [ABI], and [TypeDefKind] interfaces.
 //
