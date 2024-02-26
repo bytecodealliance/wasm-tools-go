24d224f0c8ddc9c5b593451c6046819ace124957
Randy Reddig <randy.reddig@fastly.com>
2024-02-26 08:48:53 -0800

    cm: update Tuple to use F0...FN field names

--- cm/tuple.go
+++ cm/tuple.go
@@ -4,25 +4,25 @@ package cm
 //
 // [Component Model tuple]: https://component-model.bytecodealliance.org/design/wit.html#tuples
 type Tuple[T0, T1 any] struct {
-	V0 T0
-	V1 T1
+	F0 T0
+	F1 T1
 }
 
 // Tuple3 represents a [Component Model tuple] with 3 fields.
 //
 // [Component Model tuple]: https://component-model.bytecodealliance.org/design/wit.html#tuples
 type Tuple3[T0, T1, T2 any] struct {
-	V0 T0
-	V1 T1
-	V2 T2
+	F0 T0
+	F1 T1
+	F2 T2
 }
 
 // Tuple4 represents a [Component Model tuple] with 4 fields.
 //
 // [Component Model tuple]: https://component-model.bytecodealliance.org/design/wit.html#tuples
 type Tuple4[T0, T1, T2, T3 any] struct {
-	V0 T0
-	V1 T1
-	V2 T2
-	V3 T3
+	F0 T0
+	F1 T1
+	F2 T2
+	F3 T3
 }
