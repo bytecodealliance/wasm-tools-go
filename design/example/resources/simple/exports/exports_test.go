061dd84427eefd12f4474dd48b0af99666865def
Randy Reddig <randy.reddig@fastly.com>
2024-03-16 15:45:18 -0700

    design/example/resources/simple: more sketching out exported resources

--- design/example/resources/simple/exports/exports_test.go
+++ design/example/resources/simple/exports/exports_test.go
@@ -2,52 +2,71 @@ package exports_test
 
 import (
 	"strconv"
-	"testing"
 
 	"github.com/ydnar/wasm-tools-go/cm"
+	"github.com/ydnar/wasm-tools-go/design/example/resources/simple/exports"
 )
 
-func TestExportNumber(t *testing.T) {
-	// exports.ExportNumber[number](numberExports{})
-	// exports.ExportNumber[numberStruct](numberStructExports{})
-}
-
-func TestNumberResourceRep(t *testing.T) {
-	// _ = exports.NumberResourceRep[number, number]
-	// _ = exports.NumberResourceRep[*numberStruct, numberStruct]
-}
-
 // Value representation
-type number int32
+var (
+	_ exports.Interface       = interface1{}
+	_ exports.NumberInterface = number1(0)
+)
 
-func (n number) Value() int32        { return int32(n) }
-func (n number) String() string      { return strconv.Itoa(int(n)) }
-func (n number) ResourceDestructor() {}
-func (n number) ResourceRep() cm.Rep { return 0 /* FIXME */ }
+type interface1 struct{}
 
-type numberExports struct{}
-
-func (exports numberExports) Constructor(value int32) number { return number(value) }
-func (exports numberExports) Merge(a number, b number) number {
-	return exports.Constructor(a.Value() + b.Value())
+func (i interface1) Number(rep cm.Rep) exports.NumberInterface {
+	return cm.AsRep[number1](rep)
 }
+
+func (i interface1) NewNumber(value int32) exports.Number {
+	return exports.NumberResourceNew(number1(value))
+}
+
+func (i interface1) NumberMerge(a exports.Number, b exports.Number) exports.Number {
+	return i.NewNumber(a.ResourceRep().Value() + b.ResourceRep().Value())
+}
+
+func (i interface1) NumberChoose(a exports.NumberInterface, b exports.NumberInterface) exports.Number {
+	return i.NewNumber(a.Value() + b.Value())
+}
+
+type number1 int32
+
+func (n number1) Value() int32        { return int32(n) }
+func (n number1) String() string      { return strconv.Itoa(int(n)) }
+func (n number1) ResourceDestructor() {}
+func (n number1) ResourceRep() cm.Rep { return 0 /* FIXME */ }
 
 // Pointer representation
-type numberStruct struct {
+var (
+	_ exports.Interface       = interface2{}
+	_ exports.NumberInterface = &number2{}
+)
+
+type interface2 struct{}
+
+func (i interface2) Number(rep cm.Rep) exports.NumberInterface {
+	return cm.AsRep[*number2](rep)
+}
+
+func (i interface2) NewNumber(value int32) exports.Number {
+	return exports.NumberResourceNew(number1(value))
+}
+
+func (i interface2) NumberMerge(a exports.Number, b exports.Number) exports.Number {
+	return i.NewNumber(a.ResourceRep().Value() + b.ResourceRep().Value())
+}
+
+func (i interface2) NumberChoose(a exports.NumberInterface, b exports.NumberInterface) exports.Number {
+	return i.NewNumber(a.Value() + b.Value())
+}
+
+type number2 struct {
 	value int32
 }
 
-func (n *numberStruct) Value() int32        { return n.value }
-func (n *numberStruct) String() string      { return strconv.Itoa(int(n.value)) }
-func (n *numberStruct) ResourceDestructor() {}
-func (n *numberStruct) ResourceRep() cm.Rep { return 0 /* FIXME */ }
-
-type numberStructExports struct{}
-
-func (exports numberStructExports) Constructor(value int32) *numberStruct {
-	return &numberStruct{value}
-}
-
-func (exports numberStructExports) Merge(a, b *numberStruct) *numberStruct {
-	return exports.Constructor(a.Value() + b.Value())
-}
+func (n *number2) Value() int32        { return n.value }
+func (n *number2) String() string      { return strconv.Itoa(int(n.value)) }
+func (n *number2) ResourceDestructor() {}
+func (n *number2) ResourceRep() cm.Rep { return 0 /* FIXME */ }
--- design/example/resources/simple/exports/simple.wit.go
+++ design/example/resources/simple/exports/simple.wit.go
@@ -5,74 +5,73 @@ import (
 )
 
 // TODO: make this a cm.Handle[T]
-type NumberHandle cm.Resource
+type Number cm.Resource
 
 // NumberResourceNew represents the imported function "[export]example:resources/simple#[resource-new]number".
 //
-// Create a new Component Model resource handle for [Number].
+// Create a new Component Model resource handle for [NumberInterface].
 //
 //go:nosplit
-func NumberResourceNew(i Number) NumberHandle {
+func NumberResourceNew(i NumberInterface) Number {
 	return wasmimport_NumberResourceNew(i.ResourceRep())
 }
 
 //go:wasmimport [export]example:resources/simple [resource-new]number
 //go:noescape
-func wasmimport_NumberResourceNew(rep cm.Rep) NumberHandle
+func wasmimport_NumberResourceNew(rep cm.Rep) Number
 
 // ResourceRep represents the the Canonical ABI function "resource-rep".
 //
-// Return a rep for [Number] from handle.
-// Caller is responsible for converting to a concrete implementation of [Number]
+// Return a [NumberInterface] from a resource handle.
 //
 //go:nosplit
-func (self NumberHandle) ResourceRep() cm.Rep {
-	return self.wasmimport_ResourceRep()
+func (self Number) ResourceRep() NumberInterface {
+	return impl.Number(self.wasmimport_ResourceRep())
 }
 
 //go:wasmimport [export]example:resources/simple [resource-rep]number
 //go:noescape
-func (self NumberHandle) wasmimport_ResourceRep() cm.Rep
+func (self Number) wasmimport_ResourceRep() cm.Rep
 
 // ResourceDrop represents represents the Canonical ABI function "resource-drop".
 //
 // Drops a resource handle.
 //
 //go:nosplit
-func (self NumberHandle) ResourceDrop() {
+func (self Number) ResourceDrop() {
 	self.wasmimport_ResourceDrop()
 }
 
 //go:wasmimport example:resources/simple [resource-drop]number
 //go:noescape
-func (self NumberHandle) wasmimport_ResourceDrop()
+func (self Number) wasmimport_ResourceDrop()
 
 //go:wasmexport example:resources/simple#[constructor]number
-func wasmexport_NumberConstructor(value int32) NumberHandle {
-	return impl.Number().Constructor(value)
+func wasmexport_NewNumber(value int32) Number {
+	return impl.NewNumber(value)
 }
 
 //go:wasmexport example:resources/simple#[static]number.merge
-func wasmexport_NumberMerge(a NumberHandle, b NumberHandle) NumberHandle {
-	return impl.Number().Merge(a, b)
+func wasmexport_NumberMerge(a Number, b Number) Number {
+	return impl.NumberMerge(a, b)
 }
 
 //go:wasmexport example:resources/simple#[static]number.choose
-func wasmexport_NumberChoose(a cm.Rep, b cm.Rep) NumberHandle {
-	return impl.Number().Choose(impl_Number(a), impl_Number(b))
+func wasmexport_NumberChoose(a cm.Rep, b cm.Rep) Number {
+	return impl.NumberChoose(impl.Number(a), impl.Number(b))
 }
 
 //go:wasmexport example:resources/simple#[method]number.value
 func wasmexport_NumberValue(rep cm.Rep) int32 {
-	return impl_Number(rep).Value()
+	return impl.Number(rep).Value()
 }
 
 //go:wasmexport example:resources/simple#[method]number.string
 func wasmexport_NumberString(rep cm.Rep, result *string) {
-	*result = impl_Number(rep).String()
+	*result = impl.Number(rep).String()
 }
 
-type Number interface {
+type NumberInterface interface {
 	ResourceRep() cm.Rep
 	ResourceDestructor()
 	Value() int32
@@ -80,21 +79,14 @@ type Number interface {
 }
 
 type Interface interface {
-	Number() interface {
-		Constructor(value int32) NumberHandle
-		Merge(a NumberHandle, b NumberHandle) NumberHandle
-		Choose(a Number, b Number) NumberHandle
-	}
+	Number(rep cm.Rep) NumberInterface
+	NewNumber(value int32) Number
+	NumberMerge(a Number, b Number) Number
+	NumberChoose(a NumberInterface, b NumberInterface) Number
 }
 
-func Export[T0 Number](i Interface) {
+func Export(i Interface) {
 	impl = i
-	impl_Number = func(rep cm.Rep) Number {
-		return cm.AsRep[T0](rep)
-	}
 }
 
-var (
-	impl        Interface
-	impl_Number func(cm.Rep) Number
-)
+var impl Interface
