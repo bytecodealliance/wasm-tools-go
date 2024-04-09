ef35e8dabfa407430283ed8763d33429ead9c29b
Randy Reddig <randy.reddig@fastly.com>
2024-04-09 14:38:53 -0700

    internal/iterate: add Seq and Seq2 types

--- internal/iterate/iterate.go
+++ internal/iterate/iterate.go
@@ -1,5 +1,23 @@
 package iterate
 
+// Seq is an iterator over sequences of individual values.
+// When called as seq(yield), seq calls yield(v) for each value v in the sequence,
+// stopping early if yield returns false.
+//
+// TODO: delete this once [GOEXPERIMENT=rangefunc] lands.
+//
+// [GOEXPERIMENT=rangefunc]: https://go.dev/wiki/RangefuncExperiment
+type Seq[V any] func(yield func(V) bool)
+
+// Seq2 is an iterator over sequences of pairs of values, most commonly key-value pairs.
+// When called as seq(yield), seq calls yield(k, v) for each pair (k, v) in the sequence,
+// stopping early if yield returns false.
+//
+// TODO: delete this once [GOEXPERIMENT=rangefunc] lands.
+//
+// [GOEXPERIMENT=rangefunc]: https://go.dev/wiki/RangefuncExperiment
+type Seq2[K, V any] func(yield func(K, V) bool)
+
 // Done wraps yield and calls done when yield returns false.
 func Done[V any](yield func(V) bool, done func()) func(V) bool {
 	return func(v V) bool {
