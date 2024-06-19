c4150eb4ff1345af9d0c183d25d428f7dea18149
Randy Reddig <randy.reddig@fastly.com>
2024-06-19 15:18:22 -0700

    cm: clean up lowering functions

--- cm/abi.go
+++ cm/abi.go
@@ -47,43 +47,21 @@ func LiftList[L List[T], T any, Data unsafe.Pointer | uintptr | *T, Len uint | u
 
 // Lower functions
 // func BoolToS32[B ~bool](v B) int32  { return int32(*(*int8)(unsafe.Pointer(&v))) }
-func BoolToU32[B ~bool](v B) uint32 {
-	return uint32(*(*uint8)(unsafe.Pointer(&v)))
-}
+func BoolToU32[B ~bool](v B) uint32 { return uint32(*(*uint8)(unsafe.Pointer(&v))) }
+func LowerBool[B ~bool](v B) uint32 { return uint32(*(*uint8)(unsafe.Pointer(&v))) }
 
-func LowerBool[B ~bool](v B) uint32 {
-	return uint32(*(*uint8)(unsafe.Pointer(&v)))
-}
-
-// func BoolToS64[B ~bool](v B) int64  { return int64(*(*int8)(unsafe.Pointer(&v))) }
 func BoolToU64[B ~bool](v B) uint64 { return uint64(*(*uint8)(unsafe.Pointer(&v))) }
 
-// func BoolToF32(v bool) float32 { return U32ToF32(BoolToU32(v)) }
-// func BoolToF64(v bool) float64 { return U32ToF64(BoolToU32(v)) }
-
-// func S8ToF32(v int8) float32   { return S32ToF32(int32(v)) }
-// func S8ToF64(v int8) float64   { return S32ToF64(int32(v)) }
-// func S16ToF32(v int16) float32 { return S32ToF32(int32(v)) }
-// func S16ToF64(v int16) float64 { return S32ToF64(int32(v)) }
-func S32ToF32(v int32) float32 { return *(*float32)(unsafe.Pointer(&v)) }
-
-// func S32ToF64(v int32) float64 { return float64(*(*float32)(unsafe.Pointer(&v))) } // FIXME: wrong
-func S64ToF64(v int64) float64 { return *(*float64)(unsafe.Pointer(&v)) }
-
-// func U8ToF32(v uint8) float32   { return U32ToF32(uint32(v)) }
-// func U8ToF64(v uint8) float64   { return U32ToF64(uint32(v)) }
-// func U16ToF32(v uint16) float32 { return U32ToF32(uint32(v)) }
-// func U16ToF64(v uint16) float64 { return U32ToF64(uint32(v)) }
-func U32ToF32(v uint32) float32 { return *(*float32)(unsafe.Pointer(&v)) }
-
-// func U32ToF64(v uint32) float64 { return float64(*(*float32)(unsafe.Pointer(&v))) }
-func U64ToF64(v uint64) float64 { return *(*float64)(unsafe.Pointer(&v)) }
-
+func S32ToF32(v int32) float32   { return *(*float32)(unsafe.Pointer(&v)) }
+func S64ToF64(v int64) float64   { return *(*float64)(unsafe.Pointer(&v)) }
+func F64ToU64(v float64) uint64  { return *(*uint64)(unsafe.Pointer(&v)) }
+func U32ToF32(v uint32) float32  { return *(*float32)(unsafe.Pointer(&v)) }
+func U64ToF64(v uint64) float64  { return *(*float64)(unsafe.Pointer(&v)) }
 func F32ToF64(v float32) float64 { return float64(v) }
 
-func PointerToU32[T any](v *T) uint32 { return uint32(Uptr(v)) }
-
-func Uptr[T any](v *T) uintptr { return uintptr(unsafe.Pointer(v)) }
+func PointerToPointer[T any](v *T) *T { return v }
+func PointerToU32[T any](v *T) uint32 { return uint32(uintptr(unsafe.Pointer(v))) }
+func PointerToU64[T any](v *T) uint64 { return uint64(uintptr(unsafe.Pointer(v))) }
 
 // Experimental lowering functions
 func Lower1[T0, F0 CoreTypes, T any](v *T) F0 {
