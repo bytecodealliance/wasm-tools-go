28508fb9746db50d8af81cd27b66644e1e07e504
Randy Reddig <randy.reddig@fastly.com>
2024-06-23 15:19:45 -0700

    cm: wrap Option around embedded option[T] type
    This preserves methods on option[T] on user-declared Option[T] types.

--- cm/option.go
+++ cm/option.go
@@ -1,14 +1,9 @@
 package cm
 
 // Option represents a Component Model [option<T>] type.
-// The first byte is a bool representing none or some,
-// followed by storage for the associated type T.
 //
 // [option<T>]: https://component-model.bytecodealliance.org/design/wit.html#options
-type Option[T any] struct {
-	isSome bool
-	some   T
-}
+type Option[T any] struct{ option[T] }
 
 // None returns an [Option] representing the none case,
 // equivalent to the zero value.
@@ -19,19 +14,29 @@ func None[T any]() Option[T] {
 // Some returns an [Option] representing the some case.
 func Some[T any](v T) Option[T] {
 	return Option[T]{
-		isSome: true,
-		some:   v,
+		option[T]{
+			isSome: true,
+			some:   v,
+		},
 	}
 }
 
+// option represents the internal representation of a Component Model option type.
+// The first byte is a bool representing none or some,
+// followed by storage for the associated type T.
+type option[T any] struct {
+	isSome bool
+	some   T
+}
+
 // None returns true if o represents the none case.
-func (o *Option[T]) None() bool {
+func (o *option[T]) None() bool {
 	return !o.isSome
 }
 
 // Some returns a non-nil *T if o represents the some case,
 // or nil if o represents the none case.
-func (o *Option[T]) Some() *T {
+func (o *option[T]) Some() *T {
 	if o.isSome {
 		return &o.some
 	}
