503d88bff56e2a87d5588318f50eb5b882ebf9cf
Randy Reddig <randy.reddig@fastly.com>
2024-09-13 16:49:21 -0700

    tests: add non-importable tests module for standalone tests of generated interfaces

---
+++ tests/tests.go
@@ -0,0 +1,10 @@
+// Package tests exists as a standalone module in the wasm-tools-go repository for
+// testing Go code generated from WIT using wit-bindgen-go.
+// There are no user-importable packages in this module.
+package tests
+
+import _ "github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go"
+
+//go:generate rm -rf ./generated/*
+//go:generate mkdir -p ./generated
+//go:generate go run github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go generate -o ./generated ../testdata/wasi/cli.wit.json
