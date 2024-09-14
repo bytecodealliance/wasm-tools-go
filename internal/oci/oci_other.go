61277ba9476a2e1b63eb3a209cc69692dddbd78f
Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
2024-09-13 18:32:48 -0700

    oci: exclude files if the GOOS is wasip1 or wasip2
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>

--- internal/oci/oci_other.go
+++ internal/oci/oci_other.go
@@ -1,3 +1,5 @@
+//go:build !wasip1 && !wasip2
+
 package oci
 
 import (
---
+++ internal/oci/oci_wasi.go
@@ -0,0 +1,17 @@
+//go:build wasip1 || wasip2
+
+package oci
+
+import (
+	"context"
+	"fmt"
+)
+
+// IsOCIPath checks if a given path is an OCI path
+func IsOCIPath(path string) bool {
+	return false
+}
+
+func PullWIT(ctx context.Context, path, out string) error {
+	return fmt.Errorf("OCI not supported on WASI")
+}
