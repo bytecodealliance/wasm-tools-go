60184a9e3de6112816a8cf016d628f71c1b6406c
Randy Reddig <randy.reddig@fastly.com>
2024-09-13 17:04:58 -0700

    tests: go.mod tweaks

--- tests/tests.go
+++ tests/tests.go
@@ -1,9 +1,8 @@
 // Package tests exists as a standalone module in the wasm-tools-go repository for
 // testing Go code generated from WIT using wit-bindgen-go.
 // There are no user-importable packages in this module.
-package tests
 
-import _ "github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go"
+package tests
 
 //go:generate rm -rf ./generated/*
 //go:generate mkdir -p ./generated
