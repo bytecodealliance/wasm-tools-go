1b1ed04af904882518114f122e25125b54bb46dd
Randy Reddig <randy.reddig@fastly.com>
2024-10-08 22:14:47 -0700

    cmd/wit-bindgen-go/cmd/wit: add --world argument to filter serialized WIT to a single world

--- cmd/wit-bindgen-go/cmd/wit/wit.go
+++ cmd/wit-bindgen-go/cmd/wit/wit.go
@@ -5,13 +5,24 @@ import (
 	"fmt"
 
 	"github.com/bytecodealliance/wasm-tools-go/internal/witcli"
+	"github.com/bytecodealliance/wasm-tools-go/wit"
 	"github.com/urfave/cli/v3"
 )
 
 // Command is the CLI command for wit.
 var Command = &cli.Command{
-	Name:   "wit",
-	Usage:  "reverses a WIT JSON file into WIT syntax",
+	Name:  "wit",
+	Usage: "reverses a WIT JSON file into WIT syntax",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:     "world",
+			Aliases:  []string{"w"},
+			Value:    "",
+			OnlyOnce: true,
+			Config:   cli.StringConfig{TrimSpace: true},
+			Usage:    "WIT world to generate, otherwise generate all worlds",
+		},
+	},
 	Action: action,
 }
 
@@ -24,6 +35,23 @@ func action(ctx context.Context, cmd *cli.Command) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(res.WIT(nil, ""))
+	var w *wit.World
+	world := cmd.String("world")
+	if world != "" {
+		w = findWorld(res, world)
+		if w == nil {
+			return fmt.Errorf("world %s not found", world)
+		}
+	}
+	fmt.Print(res.WIT(w, ""))
+	return nil
+}
+
+func findWorld(r *wit.Resolve, pattern string) *wit.World {
+	for _, w := range r.Worlds {
+		if w.Match(pattern) {
+			return w
+		}
+	}
 	return nil
 }
